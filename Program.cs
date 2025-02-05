5c3234abd42ec0bc97c3e18fa89031f5b14833af
Madumqs07 <mariaeduardamqs.sesi@gmail.com>
2025-02-05 12:02:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Net.NetworkInformation;
+
+namespace _03_02_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        //exercício 1
+
+        { /*
+            System.Console.WriteLine("Insira o primeiro número: ");
+            var n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o segundo número: ");
+            var n2 = int.Parse(Console.ReadLine());
+
+            var soma = n1 + n2;
+            var subt = n1 - n2;
+            var mult = n1 * n2;
+            double div = n1 / n2;
+
+            System.Console.WriteLine($"Soma:  {soma}");
+            System.Console.WriteLine($"Subtração:  {subt}");
+            System.Console.WriteLine($"Multiplicação:  {mult}");
+            System.Console.WriteLine($"Divisão:  {div}"); */
+
+
+            //exercício 2
+
+            /* System.Console.WriteLine("Insira o preço do produto: ");
+             float valor = float.Parse(Console.ReadLine());
+
+             double desconto = (valor * 0.2);
+
+             System.Console.WriteLine($"O desconto é de: {desconto}"); */
+
+            //exercício 3
+
+            /* for(int numero3 = 0; numero3 < 20; numero3 ++) { 
+
+                 if(numero3 % 2 == 0)
+                 {
+
+                     System.Console.WriteLine($"{numero3}"); 
+                 }
+
+             }*/
+
+            //exercício 4
+
+            /*System.Console.WriteLine("Insira um número: ");
+            var numero4 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Insira o segundo número: ");
+            var numero5 = int.Parse(Console.ReadLine());
+
+            int soma1 = 0;
+
+            for(int i = numero4; i <= numero5; i++) {
+                soma1 += i;
+                    
+            }
+            System.Console.WriteLine($"A soma dos números no intervalo do número {numero4} e o número {numero5} é {soma1}");
+             */
+
+            //exercício 5
+
+            /* int[] vetor= new int[5];
+
+             for (int i1 = 0;i1 < 5; i1++)
+             {
+                 System.Console.WriteLine($"Insira o {i1 + 1}°:   ");
+                 var numero6 = int.Parse(Console.ReadLine());
+                 vetor[i1] = numero6;
+
+
+             }
+
+             for (int i2 = 4; i2 >= 0; i2--)
+             {
+
+                System.Console.WriteLine($"{vetor[i2]}");
+
+             }
+            */
+
+            //exercício 6
+
+            /* int[] vetor1 = new int[5];
+
+             for (int i3 = 0; i3 < 5; i3++)
+             {
+                 System.Console.WriteLine($"Insira o {i3 + 1}° número: ");
+                 var numero7 = int.Parse(Console.ReadLine());
+                 vetor1[i3] = numero7;
+             }
+
+             for (int i4 = 0; i4 < 5; i4++) { 
+             System.Console.WriteLine("Insira um número para conferir se esta no vetor: ");
+             var numero8 = int.Parse(Console.ReadLine());
+
+             if (numero8 == vetor1[i4])
+             {
+                 System.Console.WriteLine($"Este número está na posição {i4 + 1} do vetor");
+                 return;
+             }
+             else
+             {
+                 System.Console.WriteLine($"Este número não está na posição {i4 + 1} do vetor");
+
+             }
+         }*/
+
+            //exercício 7
+
+            /* System.Console.WriteLine("Insira a 1° nota: ");
+             float numero9 = float.Parse(Console.ReadLine());
+             System.Console.WriteLine("Insira a 2° nota: ");
+             float numero10 = float.Parse(Console.ReadLine());
+             System.Console.WriteLine("Insira a 3° nota: ");
+             float numero11 = float.Parse(Console.ReadLine());
+
+             var media = (numero9 + numero10 + numero11) / 3;
+
+             System.Console.WriteLine($"A média dessas notas é {media}");
+
+         }*/
+
+            //exercício 8
+
+           /* System.Console.WriteLine("Insira um número: ");
+            var numero12 = int.Parse(Console.ReadLine());
+
+            for(int i5 = 0; i5 < 10; i5++)
+            {
+                if (numero12 % (i5 + 1) == 0)
+                {
+                    System.Console.WriteLine($"O número {numero12} não é primo.");
+                    return;
+                }
+
+                
+                
+                    System.Console.WriteLine($"O número {numero12}  é primo.");*/
+                    
+                }
+
+            }
+            
+
+    }                                             
+    }  
